f150b780bda2ff24106ed56d78f09842b1f5d591
Gino <mazza.gino32@gmail.com>
2024-09-13 19:07:14 -0300

    Primera implementacion del rehash, aun no funciona

--- hash.cpp
+++ hash.cpp
@@ -16,16 +16,68 @@ class THash{
     int totales;
     int habilitados;
 
-    int fHash1(int id) {
-        return id % tam;
+    //**********************GPT PRUEBAS******************************
+    // Función para verificar si un número es primo
+    bool esPrimo(int n) {
+        if (n <= 1) return false; // Los números menores o iguales a 1 no son primos
+        if (n == 2 || n == 3) return true; // 2 y 3 son primos
+        if (n % 2 == 0 || n % 3 == 0) return false; // Números divisibles por 2 o 3 no son primos
+
+        // Probar divisores hasta la raíz cuadrada de n
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
     }
 
-    int fHash2(int id) {
-        return 1 + (id % (tam - 1));
+    // Función para encontrar el primo mayor más cercano
+    int primoMayorCercano(int n) {
+        int mayor = n + 1;
+
+        // Buscar hacia arriba el próximo primo
+        while (!esPrimo(mayor)) {
+            mayor++;
+        }
+
+        return mayor;
+    }
+    //*********************FIN GPT PRUEBAS**************************
+
+    int fHash1(int id, int tama) {
+        return id % tama;
     }
 
-    nodoHash* rehash(){
-        //Implementar
+    int fHash2(int id, int tama) {
+        return 1 + (id % (tama - 1));
+    }
+
+    void insertarRehash(nodoHash* nuevaTabla, int id, string titulo, char estado, int bucket, int tama){
+        if(nuevaTabla[bucket].estado == 'V'){
+            nodoHash nuevo;
+            nuevo.id = id;
+            nuevo.titulo = titulo;
+            nuevo.estado = estado;
+            nuevaTabla[bucket] = nuevo;
+        }
+        else {
+            int nuevoBucket = proximoVacio(nuevaTabla, id, bucket, tama);
+            insertarRehash(nuevaTabla, id, titulo, estado, nuevoBucket, tama);
+        }
+    }
+
+    void rehash(){
+        int nuevoTam = primoMayorCercano(tam); //proxPrimo?
+        nodoHash* nuevaTabla = new nodoHash[nuevoTam]();
+        for (int i = 0; i < tam; i++) {
+            if(tabla[i].estado != 'V'){
+                int bucket = fHash1(tabla[i].id, nuevoTam);
+                insertarRehash(nuevaTabla, tabla[i].id, tabla[i].titulo, tabla[i].estado, bucket, nuevoTam);
+            }
+        }
+        delete[] tabla;
+        tabla = nuevaTabla;
+        tam = nuevoTam;
     }
 
     void destruir(){
@@ -33,12 +85,14 @@ class THash{
         tabla = NULL;
     }
 
-    int proximoVacio(int bucket, int id) {
+    int proximoVacio(nodoHash* tab, int bucket, int id, int tama) {
         int i = 1;
         int nuevoBucket = bucket;
-        while (tabla[nuevoBucket].estado != 'V' && tabla[nuevoBucket].id != id && i < tam) {
-            nuevoBucket = (bucket + i * fHash2(id)) % tam;
+        bool sigo = true;
+        while (tab[nuevoBucket].estado != 'V' && tab[nuevoBucket].id != id && i < tama && sigo) {
+            nuevoBucket = (bucket + i * fHash2(id, tama)) % tama;
             i++;
+            if(i == tama) sigo = false;
         }
         return nuevoBucket;
     }
@@ -56,13 +110,13 @@ class THash{
         }
         else if(tabla[bucket].id == id){
             tabla[bucket].titulo = titulo;
-            if(tabla[bucket].estado = 'D'){
+            if(tabla[bucket].estado == 'D'){
                 tabla[bucket].estado = 'H';
                 habilitados++;
             }
         }
         else if(tabla[bucket].id != id){
-            int nuevoBucket = proximoVacio(bucket, id);
+            int nuevoBucket = proximoVacio(tabla, bucket, id, tam);
             insertarAux(id, titulo, nuevoBucket);
         }
     }
@@ -79,18 +133,18 @@ class THash{
                 habilitados--;
             }
         }
-        else if(tabla[bucket].id != id) toggleAux(id, proximoVacio(bucket, id));  
+        else if(tabla[bucket].id != id) toggleAux(id, proximoVacio(tabla, bucket, id, tam));  
     }
 
     void findAux(int id, int bucket){
         if(tabla[bucket].estado == 'V') cout << "libro_no_encontrado" << endl;
         else if(tabla[bucket].id == id) cout << tabla[bucket].titulo << " " << tabla[bucket].estado << endl;
-        else if(tabla[bucket].id != id) findAux(id, proximoVacio(bucket, id));
+        else if(tabla[bucket].id != id) findAux(id, proximoVacio(tabla, bucket, id, tam));
     }
 
     public:
     THash(int largo){
-        tabla = new nodoHash[tam]();
+        tabla = new nodoHash[largo]();
         tam = largo;
 		totales = 0;
 		habilitados = 0;
@@ -105,18 +159,18 @@ class THash{
 
     void add(int id, string titulo) {
         float fc = totales / (float)tam;
-        if(fc > 0,7) rehash();
-        int bucket = fHash1(id);
+        if(fc > 0.7) rehash();
+        int bucket = fHash1(id, tam);
         insertarAux(id, titulo, bucket);
 	}
 
     void find(int id){
-        int bucket = fHash1(id);
+        int bucket = fHash1(id, tam);
         findAux(id, bucket);
     }
 
     void toggle(int id){
-        int bucket = fHash1(id);
+        int bucket = fHash1(id, tam);
         toggleAux(id, bucket);
     }
 
