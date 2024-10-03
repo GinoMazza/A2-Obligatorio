8d9d4b7d7a8d522266afce841a768db2cba715d4
Gino <mazza.gino32@gmail.com>
2024-10-03 17:25:10 -0300

    Implementamos main en ejercicio3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -7,5 +7,16 @@
 using namespace std;
 
 int main(){
-    
+    int N = 0;
+    int K = 0;
+    cin >> N;
+    MinHeap* heap = new MinHeap(N);
+    for (int i = 0; i < N; i++){
+        int id;
+        int precio;
+        cin >> id >> precio;
+        heap->insertar(id, precio);
+    }
+    cin >> K;
+    heap->imprimirK(K);
 }
