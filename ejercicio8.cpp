0fbf78046a0e1ff80d4600fe66ffbb0fd6f51eab
Gino <mazza.gino32@gmail.com>
2024-11-12 18:07:35 -0300

    Ejercicio9 terminado, pasan todas las pruebas

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -2,11 +2,59 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include <climits>
+#include <math.h>
 
 using namespace std;
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+// Función para calcular la distancia entre dos ciudades
+int distanciaEfectiva(int ciudad1, int ciudad2, int *x, int *y, int *poblaciones)
+{
+    int restaX = x[ciudad2] - x[ciudad1];
+    int restaY = y[ciudad2] - y[ciudad1];
+    int absRestaP = abs(poblaciones[ciudad2] - poblaciones[ciudad1]);
+    int poblacionMax = max(poblaciones[ciudad2], poblaciones[ciudad1]);
+
+    int distancia = sqrt(restaX * restaX + restaY * restaY) + (absRestaP / poblacionMax);
+
+    return distancia;
+}
+
 int main()
 {
-    // TODO
+    int n;
+    cin >> n;
+
+    // Inicializamos vectores de datos de cada ciudad
+    int *x = new int[n + 1]();
+    int *y = new int[n + 1]();
+    int *poblaciones = new int[n + 1]();
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x[i] >> y[i] >> poblaciones[i];
+    }
+
+    // Encontrar la minima distancia efectiva entre todas las ciudades usando dividir y conquistar
+    int minDistancia = INT_MAX;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            minDistancia = min(minDistancia, distanciaEfectiva(i, j, x, y, poblaciones));
+        }
+    }
+
+    //! le hablo a mi chatty a ver tambien je pelotudo >:( :D oki
+
+    delete[] x;
+    delete[] y;
+    delete[] poblaciones;
+
     return 0;
 }
--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,13 +5,86 @@
 
 using namespace std;
 
+// Función que devuelve el máximo de dos números
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+// Función mochila de 4 dimensiones
+void mochila4D(int cantJugadores, int cantExtranjeros, int cantPresupuesto, int *valoraciones, int *salarios, int *extranjeros)
+{
+    // Generamos la matriz de tabulación (4 dimensiones)
+    int ****tab = new int ***[cantJugadores + 1];
+
+    for (int jugadorActual = 0; jugadorActual <= cantJugadores; jugadorActual++)
+    {
+        tab[jugadorActual] = new int **[cantPresupuesto + 1]();
+
+        for (int presupuestoActual = 0; presupuestoActual <= cantPresupuesto; presupuestoActual++)
+        {
+            tab[jugadorActual][presupuestoActual] = new int *[cantExtranjeros + 1]();
+
+            for (int cantExtranjerosActual = 0; cantExtranjerosActual <= cantExtranjeros; cantExtranjerosActual++)
+            {
+                tab[jugadorActual][presupuestoActual][cantExtranjerosActual] = new int[12](); // 12 porque son 11 jugadores + 1
+            }
+        }
+    }
+
+    for (int jugadorActual = 1; jugadorActual <= cantJugadores; jugadorActual++)
+    {
+        // Obtenemos los datos del jugador actual
+        int valoracionJugadorActual = valoraciones[jugadorActual];
+        int salarioJugadorActual = salarios[jugadorActual];
+        int esExtranjeroJugadorActual = extranjeros[jugadorActual];
+
+        for (int presupuestoActual = 0; presupuestoActual <= cantPresupuesto; presupuestoActual++)
+        {
+            for (int cantExtranjerosActual = 0; cantExtranjerosActual <= cantExtranjeros; cantExtranjerosActual++)
+            {
+                for (int cantJugadoresMaximosActual = 1; cantJugadoresMaximosActual <= 11; cantJugadoresMaximosActual++)
+                {
+                    // Si el jugador actual no entra (ya sea por capacidad de salario o extranjero)
+                    if (salarioJugadorActual > presupuestoActual || esExtranjeroJugadorActual > cantExtranjerosActual)
+                    {
+                        tab[jugadorActual][presupuestoActual][cantExtranjerosActual][cantJugadoresMaximosActual] = tab[jugadorActual - 1][presupuestoActual][cantExtranjerosActual][cantJugadoresMaximosActual];
+                    }
+
+                    // Entra
+                    else
+                    {
+                        // Comparamos valores de usar y no usar al jugador actual
+                        int valorDeUsar = valoracionJugadorActual + tab[jugadorActual - 1][presupuestoActual - salarioJugadorActual][cantExtranjerosActual - esExtranjeroJugadorActual][cantJugadoresMaximosActual - 1];
+
+                        int valorDeNoUsar = tab[jugadorActual - 1][presupuestoActual][cantExtranjerosActual][cantJugadoresMaximosActual];
+
+                        // Se guarda el máximo de los valores
+                        tab[jugadorActual][presupuestoActual][cantExtranjerosActual][cantJugadoresMaximosActual] = max(valorDeUsar, valorDeNoUsar);
+                    }
+                }
+            }
+        }
+    }
+
+    // El valor máximo alcanzado con los 11 jugadores seleccionados
+    int valoracionTotal = tab[cantJugadores][cantPresupuesto][cantExtranjeros][11];
+
+    // Devolvemos el promedio
+    int promedioValoracion = valoracionTotal / 11;
+    cout << promedioValoracion << endl;
+}
+
 int main()
 {
     int j, presupuesto, cantExtranjeros;
     cin >> j;
+
+    // Inicializamos vectores
     int *valoraciones = new int[j + 1]();
-    int *costos = new int[j + 1]();
-    bool *extranjeros = new bool[j + 1]();
+    int *salarios = new int[j + 1]();
+    int *extranjeros = new int[j + 1]();
+
     for (int i = 1; i <= j; i++)
     {
         int valoracion = 0;
@@ -33,11 +106,20 @@ int main()
             else if (confianza == "Baja")
                 valoracion -= 5;
         }
+        // Guardamos datos de cada jugador
         valoraciones[i] = valoracion;
-        costos[i] = costo;
-        extranjeros[i] = extranjero == "Si" ? true : false;
+        salarios[i] = costo;
+        extranjeros[i] = extranjero == "Si" ? 1 : 0;
     }
-    cin >> presupuesto >> cantExtranjeros;
-}
 
-// return final;
+    cin >> presupuesto >> cantExtranjeros;
+
+    // LLamamos a la mochila
+    mochila4D(j, cantExtranjeros, presupuesto, valoraciones, salarios, extranjeros);
+
+    delete[] valoraciones;
+    delete[] salarios;
+    delete[] extranjeros;
+
+    return 0;
+}
